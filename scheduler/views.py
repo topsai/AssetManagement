ef5449db28b02a43f8cc1c30b77590ea68330d11
fan <hurte@foxmail.com>
2017-12-29 17:28:22 +0800

    ubutun up

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,29 +1,67 @@
 from django.shortcuts import render
 import requests
 from plugs import jenkins_plug
-import paramiko
+# import paramiko
 import configparser
+from configparser import ConfigParser
+from io import StringIO
 
+
+# from ConfigParser import ConfigParser
+# 配置文件类
+class config:
+    def __init__(self):
+        self.user = None
+
+    def global_conf(self):
+        user= 'www-data'
+        worker_processes = 'auto'
+        pid = '/run/nginx.pid'
+        include = '/etc/nginx/modules-enabled/*.conf'
+
+    def events(self):
+        worker_connections = 768
+        multi_accept = 'on'
+
+
+
+# 切配置文件
+def cut_conf(file):
+    conf_dict = []
+    with open(file, 'r') as f:
+        for i in f:
+            line = i.strip().strip(';')
+            # 去除已注释配置
+            if line.startswith('#'):
+                continue
+            line = line.split()
+            # 去除无效行
+            if len(line) < 2:
+                continue
+            print(len(line))
+            conf_dict.append(line)
+    return conf_dict
 
 # Create your views here.
 # 调度器管理
 def nginx(request):
     cf = configparser.ConfigParser()
     # 建立一个sshclient对象
-    ssh = paramiko.SSHClient()
-    # 允许将信任的主机自动加入到host_allow 列表，此方法必须放在connect方法的前面
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    # 调用connect方法连接服务器
-    ssh.connect(hostname='192.168.202.143', port=22, username='root', password='cai@521131')
-    stdin, stdout, stderr = ssh.exec_command('cat /etc/nginx/nginx.conf')
-    # 结果放到stdout中，如果有错误将放到stderr中
-    ssh_ret = stdout.read().decode()
-    print('config:', ssh_ret)
-    cf.read(ssh_ret)
-    conf = cf.sections()
-    print('conf:', conf)
+    # ssh = paramiko.SSHClient()
+    # # 允许将信任的主机自动加入到host_allow 列表，此方法必须放在connect方法的前面
+    # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # # 调用connect方法连接服务器
+    # ssh.connect(hostname='192.168.202.143', port=22, username='root', password='cai@521131')
+    # stdin, stdout, stderr = ssh.exec_command('cat /etc/nginx/nginx.conf')
+    # # 结果放到stdout中，如果有错误将放到stderr中
+    # ssh_ret = stdout.read().decode()
+    # print('config:', ssh_ret)
     # 关闭连接
-    ssh.close()
+    # ssh.close()
+    conf_file = '/etc/nginx/nginx.conf'
+    conf = cut_conf(conf_file)
+    print('conf:', conf)
+
     # client = docker.APIClient(base_url='http://192.168.202.143:2375')
     ret = requests.get('http://192.168.202.143:18099/nginx_status')
     # data = models.User.objects.all()
