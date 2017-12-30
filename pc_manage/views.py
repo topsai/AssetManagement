2c9a2674905aecc1eab2f265289b4163d6d15733
sai <hurte@foxmail.com>
2017-12-31 00:18:38 +0800

    up

--- pc_manage/views.py
+++ pc_manage/views.py
@@ -228,7 +228,8 @@ class MyParamiko:
 # Create your views here.
 def index(request):
     client = docker.APIClient(base_url='http://192.168.202.143:2375')
-    ret = requests.get('http://192.168.202.143:18099/nginx_status')
+    # ret = requests.get('http://192.168.202.143:18099/nginx_status')
+    ret = []
     data = models.User.objects.all()
     ret = ret.text.split()
     ret = [ret[2], ret[7], ret[8], ret[9], ret[11], ret[13], ret[15]]
